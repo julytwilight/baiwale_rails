63afe491f0eb68bd088638942415b61da442cc0c
2forVendetta <julytwilight@msn.com>
2013-06-05 10:07:56 +0800

    d

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,6 +47,7 @@ Baiwale::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w[ admin_layout.css admin_style.css ]
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
